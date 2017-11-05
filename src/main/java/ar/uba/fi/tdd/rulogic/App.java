192404e875dc98fa8365150556ce6b189bf74fab
Franco <franverri@hotmail.com>
2017-11-05 18:01:48 -0300

    Ciclo de recepción de consultas de la app

--- src/main/java/ar/uba/fi/tdd/rulogic/App.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/App.java
@@ -1,12 +1,33 @@
 package ar.uba.fi.tdd.rulogic;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Console application.
  *
  */
 public class App
 {
+	private static boolean consultacorrecta(String consulta) {
+		return consulta.matches("^[a-z]*\\([a-z,]*\\)");
+	}
+	
 	public static void main(String[] args) {
+		System.out.println("-------INICIO DEL INTERPRETE-------");
 		System.out.println("I shall answer all your questions!");
+		String consulta = "";
+		Scanner scanner = new Scanner(System.in);
+		do {
+			System.out.println("Ingrese su consulta o 'q' para salir: ");
+			consulta = scanner.nextLine().replaceAll("\\s+","");
+			if(consultacorrecta(consulta)) {
+				System.out.println("SI");
+			} else if (!consulta.equals("q")){
+				System.out.println("Sintaxis de la consulta inválida");
+			}
+		} while (!consulta.equals("q"));
+		System.out.println("--------FIN DEL INTERPRETE--------");
+		scanner.close();
     }
 }
